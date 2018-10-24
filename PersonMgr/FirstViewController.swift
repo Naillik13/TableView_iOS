7ca0dd379852c4685263f469727a9a0e0d26b1b4
killian galea <killiangalea@macbook-pro-de-killian.local>
2018-10-24 09:05:38 +0200

    Initial Commit

---
+++ PersonMgr/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  PersonMgr
+//
+//  Created by killian galea on 24/10/2018.
+//  Copyright © 2018 killiangalea. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ PersonMgr/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PersonMgr
+//
+//  Created by killian galea on 24/10/2018.
+//  Copyright © 2018 killiangalea. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
